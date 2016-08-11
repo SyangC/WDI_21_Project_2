7b480d021906f7026eab17b3345a46d5663d1234
Shu-yang Chia <shuyangchia@hotmail.co.uk>
2016-08-11 17:54:40 +0100

    sorted out styling on the book show page. I now need to sort out styling for the profile page and sort out the links in the recommendations page

--- app/uploaders/book_image_uploader.rb
+++ app/uploaders/book_image_uploader.rb
@@ -36,6 +36,10 @@ class BookImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [50, 50]
   end
 
+  version :cover do
+    process :resize_to_fill => [270, 450]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
